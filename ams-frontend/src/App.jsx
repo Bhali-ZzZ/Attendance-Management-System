443d620e9a6f9f31a37b307543e1a779898f4e0b
M Bilal <bilalxd512@gmail.com>
2024-08-07 20:46:27 +0500

    commit

--- ams-frontend/src/App.jsx
+++ ams-frontend/src/App.jsx
@@ -24,7 +24,7 @@ import './styles/about.css';
 const App = () => {
   return (
     <Router>
-      <div>
+      <div className='background'>
         <Navbar />
         <div className="dashboard-container">
           <Sidebar />
